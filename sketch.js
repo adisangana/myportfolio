a2dd24578e049f76b68963d027c76ec63a586b7a
adisa <adisangana@gmail.com>
2024-10-16 13:28:43 -0500

    gogorevert

--- sketch.js
+++ sketch.js
@@ -4,18 +4,19 @@ function setup() {
     let canvas = createCanvas(windowWidth, windowHeight);
     canvas.parent('sketch-holder');
 
-    // Create a larger number of particles for a finer, dust-like effect
-    for (let i = 0; i < 300; i++) {
+    // Create an initial set of particles
+    for (let i = 0; i < 150; i++) {
         particles.push(new Particle());
     }
 }
 
 function draw() {
-    background(0, 0, 0, 10); // Very subtle dark background for more depth
+    background(0, 0, 0, 25); // Slightly transparent black for smooth blending
 
     // Update and display all particles
     for (let i = 0; i < particles.length; i++) {
         particles[i].update();
+        particles[i].attractToMouse();
         particles[i].show();
     }
 }
@@ -24,18 +25,27 @@ function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
 }
 
-// Particle class for fine dust-like effect
+// Particle class
 class Particle {
     constructor() {
+        // Initial position and velocity of particles
         this.pos = createVector(random(width), random(height));
-        this.vel = createVector(random(-0.5, 0.5), random(-0.5, 0.5)); // Slower velocity for fine particles
-        this.size = random(2, 5); // Smaller particle size for fine dust effect
+        this.vel = createVector(random(-2, 2), random(-2, 2));
+        this.acc = createVector(0, 0);
+        this.size = random(5, 15);
+        this.maxSpeed = 4;
     }
 
     update() {
+        // Apply acceleration to velocity
+        this.vel.add(this.acc);
+        this.vel.limit(this.maxSpeed);
         this.pos.add(this.vel);
 
-        // Bounce particles at the edges
+        // Reset acceleration each frame
+        this.acc.mult(0);
+
+        // Reverse direction if the particle hits the edges
         if (this.pos.x > width || this.pos.x < 0) {
             this.vel.x *= -1;
         }
@@ -44,9 +54,21 @@ class Particle {
         }
     }
 
+    attractToMouse() {
+        let mouse = createVector(mouseX, mouseY);
+        let force = p5.Vector.sub(this.pos, mouse); // Repel particles from mouse
+
+        let distance = force.mag();
+        distance = constrain(distance, 10, 200); 
+        let strength = (1 / (distance * distance)) * 1000; 
+        force.setMag(strength);
+
+        this.acc.add(force);
+    }
+
     show() {
         noStroke();
-        fill(255, 255, 255, 100); // White, semi-transparent for dust-like particles
+        fill(255, 255, 255, 150); // White particles with some transparency
         ellipse(this.pos.x, this.pos.y, this.size);
     }
 }
