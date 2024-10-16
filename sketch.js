b633efd9f2c24aac61dbbb85f0e4fda78024e2e4
adisa <adisangana@gmail.com>
2024-10-16 15:29:55 -0500

    rahhhh

--- sketch.js
+++ sketch.js
@@ -4,19 +4,18 @@ function setup() {
     let canvas = createCanvas(windowWidth, windowHeight);
     canvas.parent('sketch-holder');
 
-    // Double the particle count
-    for (let i = 0; i < 300; i++) {
+    // Increase the number of smaller particles
+    for (let i = 0; i < 1500; i++) {
         particles.push(new Particle());
     }
 }
 
 function draw() {
-    background(0, 0, 0, 25); // Slightly transparent black for smooth blending
+    background(240, 240, 240, 25); // Light gray background with slight opacity
 
     // Update and display all particles
     for (let i = 0; i < particles.length; i++) {
         particles[i].update();
-        particles[i].repelFromMouse();
         particles[i].show();
     }
 }
@@ -25,24 +24,18 @@ function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
 }
 
-// Particle class
+// Particle class for fine dust-like particles
 class Particle {
     constructor() {
-        // Initial position and velocity of particles
         this.pos = createVector(random(width), random(height));
-        this.vel = createVector(random(-1, 1), random(-1, 1)); // Slow, natural movement
+        this.vel = createVector(random(-0.5, 0.5), random(-0.5, 0.5)); // Slow velocity for subtle movement
         this.acc = createVector(0, 0);
-        this.size = random(2, 7); // Smaller particles for fine dust effect
+        this.size = random(1, 4); // Very small particles for fine dust effect
     }
 
     update() {
         // Apply velocity to position
-        this.vel.add(this.acc);
-        this.vel.limit(2); // Limit speed for natural movement
         this.pos.add(this.vel);
-
-        // Reset acceleration each frame
-        this.acc.mult(0);
 
         // Bounce particles at the edges
         if (this.pos.x > width || this.pos.x < 0) {
@@ -53,23 +46,9 @@ class Particle {
         }
     }
 
-    // Slightly repel particles from the mouse
-    repelFromMouse() {
-        let mouse = createVector(mouseX, mouseY);
-        let force = p5.Vector.sub(this.pos, mouse); // Vector pointing away from the mouse
-        let distance = force.mag(); // Get distance between particle and mouse
-
-        // Only apply repulsion if the particle is within a certain distance
-        if (distance < 100) {
-            let strength = map(distance, 0, 100, 5, 0); // Repel stronger when closer
-            force.setMag(strength); // Set the strength of the repulsion
-            this.acc.add(force); // Apply repulsion force
-        }
-    }
-
     show() {
         noStroke();
-        fill(255, 255, 255, 150); // White particles with some transparency
+        fill(255, 255, 255, 150); // Light white particles with some transparency
         ellipse(this.pos.x, this.pos.y, this.size);
     }
 }
