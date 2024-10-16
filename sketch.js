cf172ebcda7ea536cdd32bcd0afdd0544def47ca
adisa <adisangana@gmail.com>
2024-10-16 13:13:46 -0500

    wholelottared

--- sketch.js
+++ sketch.js
@@ -5,13 +5,13 @@ function setup() {
     canvas.parent('sketch-holder');
 
     // Create an initial set of particles
-    for (let i = 0; i < 150; i++) { // Increased particle count for a denser effect
+    for (let i = 0; i < 150; i++) {
         particles.push(new Particle());
     }
 }
 
 function draw() {
-    background(240, 240, 240, 25); // Light gray background with slight transparency for a motion trail effect
+    background(0, 0, 0, 25); // Slightly transparent black for smooth blending
 
     // Update and display all particles
     for (let i = 0; i < particles.length; i++) {
@@ -31,21 +31,21 @@ class Particle {
         // Initial position and velocity of particles
         this.pos = createVector(random(width), random(height));
         this.vel = createVector(random(-2, 2), random(-2, 2));
-        this.acc = createVector(0, 0); // Acceleration will change when interacting with the mouse
-        this.size = random(10, 30); // Larger particles for more visual effect
-        this.maxSpeed = 5; // Maximum speed the particle can reach
+        this.acc = createVector(0, 0);
+        this.size = random(5, 15);
+        this.maxSpeed = 4;
     }
 
     update() {
         // Apply acceleration to velocity
         this.vel.add(this.acc);
-        this.vel.limit(this.maxSpeed); // Limit the velocity to maxSpeed
+        this.vel.limit(this.maxSpeed);
         this.pos.add(this.vel);
 
         // Reset acceleration each frame
         this.acc.mult(0);
 
-        // Reverse direction if the particle hits the edges of the canvas
+        // Reverse direction if the particle hits the edges
         if (this.pos.x > width || this.pos.x < 0) {
             this.vel.x *= -1;
         }
@@ -55,23 +55,20 @@ class Particle {
     }
 
     attractToMouse() {
-        // Create a vector pointing from the particle to the mouse
         let mouse = createVector(mouseX, mouseY);
-        let force = p5.Vector.sub(this.pos, mouse); // Now it repels from the mouse
+        let force = p5.Vector.sub(this.pos, mouse); // Repel particles from mouse
 
-        // Calculate the distance between the particle and the mouse
-        let distance = force.mag(); 
-        distance = constrain(distance, 10, 200); // Constrain distance to prevent too fast speeds
-        let strength = (1 / (distance * distance)) * 1000; // Adjust the strength of repulsion
-        force.setMag(strength); // Set the magnitude of the force based on distance
+        let distance = force.mag();
+        distance = constrain(distance, 10, 200); 
+        let strength = (1 / (distance * distance)) * 1000; 
+        force.setMag(strength);
 
-        // Apply the force to the particle's acceleration
         this.acc.add(force);
     }
 
     show() {
         noStroke();
-        fill(50, 150, 200, 200); // Adjusted color to a more vibrant blue
+        fill(255, 255, 255, 150); // White particles with some transparency
         ellipse(this.pos.x, this.pos.y, this.size);
     }
 }
